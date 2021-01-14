6fbf1d74bad66c1a687fcb4201efb6d8a6071c65
Ali Saadati <saadati944@gmail.com>
2021-01-14 21:34:05 +0330

    add detect_displacement function in beadtracker.py and print blobs displacements

--- beadtracker.py
+++ beadtracker.py
@@ -1,11 +1,40 @@
 import sys
 import beadfinder
 
+delta = 0
+
+
+def detect_displacement(beads1, beads2):
+    '''
+    calculate the displacement of each blob between two frames
+    '''
+    global delta
+
+    for i in range(len(beads1)):
+        
+        min_distance = delta+1
+
+        # find minimum distance between the
+        # beads[i] and one of the beads2
+        for j in range(len(beads2)):
+            #calculate distance
+            d = beads1[i].distanceTo(beads2[j])
+            if d < min_distance and d <= delta:
+                min_distance = d
+        
+        # print min_distance only when the blob
+        # moves less than the delta
+        if min_distance != delta+1:
+            print(min_distance)
+
+
 def main():
+    global delta
     '''
     get min_pixels, tau, delta, filenames ...
     from the standard input and trace beads movements
     '''
+    # get data from stdin
     min_pixels = int(sys.argv[1])
     tau = float(sys.argv[2])
     delta = float(sys.argv[3])
@@ -17,10 +46,9 @@ def main():
         bf = beadfinder.BeadFinder(f, tau)
         blobs.append(bf.getBeads(min_pixels))
 
-    for beads in blobs:
-        print("+++++++++++++++++++++++++++++++")
-        for b in beads:
-            print(b)
+    # pass each to continues frame to detect_displacement function
+    for i in range(len(blobs)-1):
+        detect_displacement(blobs[i], blobs[i+1])
 
 
 if __name__ == "__main__":
